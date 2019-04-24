568716d5f5f9af5f65272f4444d95fc0b84b7ba9
linze <linze@tuzhanai.com>
2019-04-24 19:11:36 +0800

    ADDED: add webpack & preload

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.bundle.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  }
+};
